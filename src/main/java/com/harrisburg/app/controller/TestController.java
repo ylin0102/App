3ce5220ddb1c6cc475fc4941f8dfc41ce6d919f7
Yuchen Lin <lynchenchen2017@gmail.com>
2021-04-01 15:36:24 -0400

    Trigger Pipeline update controller

--- src/main/java/com/harrisburg/app/controller/TestController.java
+++ src/main/java/com/harrisburg/app/controller/TestController.java
@@ -2,6 +2,7 @@ package com.harrisburg.app.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,6 @@ public class TestController {
 
     @GetMapping("test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Hello world1");
+        return ResponseEntity.ok("Hello world! Hey");
     }
 }
